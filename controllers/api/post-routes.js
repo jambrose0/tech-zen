ff7b5f1a985723aa0063f95c358ede7c273597b2
jambrose0 <jambrose0518@gmail.com>
2022-05-14 14:21:05 -0400

    added routes and refactored them

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,24 +1,13 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Comment, Vote } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
+    attributes: ["id", "post_url", "title", "created_at"],
     include: [
       {
         model: Comment,
@@ -46,18 +35,7 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
+    attributes: ["id", "post_url", "title", "created_at"],
     include: [
       {
         model: Comment,
@@ -87,26 +65,12 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
     user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.put("/upvote", withAuth, (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(
-    { ...req.body, user_id: req.session.user_id },
-    { Vote, Comment, User }
-  )
-    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
